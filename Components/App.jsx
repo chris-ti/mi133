56ec640487bc1cd28a06edbf2507dc4558c24bb8
chris-ti <37975715+chris-ti@users.noreply.github.com>
2018-04-03 15:19:56 +0200

    fixed leftover import

--- Components/App.jsx
+++ Components/App.jsx
@@ -2,11 +2,8 @@ import {createStore} from "redux"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Loginview from './Loginview'
-import reducer from '../model/reducer'
 
-//isloogedin--> isadmin, conditional rendering here?
-
-const store = createStore(reducer);
+//const store = createStore(reducer);
 
 export default class App extends React.Component {
     render() {
